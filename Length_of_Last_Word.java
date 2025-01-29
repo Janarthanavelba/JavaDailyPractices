7eb0a723de1a2acf5edb9a8589bc7cd15a5a7723
Janarthanavel K <129484927+Janarthanavelba@users.noreply.github.com>
2025-01-29 20:48:01 +0530

    Create Length_of_Last_Word.java

---
+++ Length_of_Last_Word.java
@@ -0,0 +1,18 @@
+//Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        String ss=s.trim();
+
+        int count=0;
+
+        for(int i=ss.length()-1;i>=0;i--)
+        {
+            if(ss.charAt(i)==' ')  break;
+            count++;
+        }
+
+        return count;
+    }
+}
