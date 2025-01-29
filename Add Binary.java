8cab50f8d6adee3c639afd91571300632dddd672
Janarthanavel K <129484927+Janarthanavelba@users.noreply.github.com>
2025-01-29 21:04:40 +0530

    Create Add Binary.java

---
+++ Add Binary.java
@@ -0,0 +1,16 @@
+//Given two binary strings a and b, return their sum as a binary string.
+//Using BigInteger variable
+
+import java.math.BigInteger; //Must Import java.math.BigInteger package
+
+class Solution {
+    public String addBinary(String a, String b) {
+        
+        BigInteger num1 = new BigInteger(a, 2);  
+        BigInteger num2 = new BigInteger(b, 2);
+
+       BigInteger res = num1.add(num2);
+
+        return res.toString(2);
+    }
+}
