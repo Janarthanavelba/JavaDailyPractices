f35c4400e058b6d7db23fe5af69c4ae77228f276
Janarthanavel K <129484927+Janarthanavelba@users.noreply.github.com>
2025-06-16 21:36:38 +0530

    Create DRF assignment.java

---
+++ DRF assignment.java
@@ -0,0 +1,15 @@
+// Function for ODD or EVEN
+public static void OddOrEven(int n)
+  {
+  if(n%2==0) System.out.print("EVEN");
+  else System.out.print("ODD");
+  }
+
+// Function for N natural numbers
+public static void NaturalNumber(int n)
+  {
+  for(int i=1;i<=n;i++)
+  {
+    System.out.print(i+" ");
+  }
+  }
